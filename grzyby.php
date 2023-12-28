20a3d1a22f8ce2fb64fbaec03c18753de02388e9
Fiedor <stachfie@wp.pl>
2023-12-28 15:09:13 +0100

    HTML done

---
+++ grzyby.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Grzybobranie</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+    <header>
+        <div id="tytul">
+            <h1>Czas na grzyby!</h1>
+        </div>
+        <div id="miniatura">
+            <a href="podgrzybek.jpg"><img src="podgrzybek-miniatura.jpg" alt="Grzybobranie"></a>
+        </div>
+    </header>
+    <main>
+        <div id="lewy">
+            <h3>Grzyby jadalne</h3>
+            <?php
+            //skrypt1
+            ?>
+            <h3>Polecamy do zup</h3>
+            <ul>
+                <?php
+                //skrypt2
+                ?>
+            </ul>
+        </div>
+        <div id="prawy">
+            <?php
+            //skrypt3
+            ?>
+        </div>
+    </main>
+    <footer>
+        <p>AUTOR: PESEL</p>
+    </footer>
+</body>
+
+</html>
